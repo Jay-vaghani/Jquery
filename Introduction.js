d7c62d4466c152b400e656e8b4b3eb2207f70a35
Jay-vaghani <jayvaghani10@gmail>
2022-09-25 13:35:53 +0530

    commit 1

---
+++ Introduction.js
@@ -0,0 +1,19 @@
+// ************************************** What Is Jquery ************************************** 
+// (1) Light Weight Javascript Library To Do More While Wright Less 
+// (2) Library Contains Different Javascript Function To Do DOM Manipulation  
+
+// ************************************** Why Jquery ************************************** 
+// (1) Short Selectors 
+// (2) Simple Syntax 
+
+// ************************************** Jquery Basic Syntax ************************************** 
+
+// $(document).ready(function(){
+//     alert(1)
+// })
+
+// $(document).ready(function(){
+//     let box1 = $("#box-1")
+//     console.log(box1);
+    
+// })
