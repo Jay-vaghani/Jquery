551fb57837018faf1573a58c99ba92ec0cf14f8f
Jay-vaghani <jayvaghani10@gmail>
2022-09-26 08:54:09 +0530

    commit

--- Introduction.js
+++ Introduction.js
@@ -9,7 +9,7 @@
 // ************************************** Jquery Basic Syntax ************************************** 
 
 // $(document).ready(function(){
-//     alert(1)
+// // Code
 // })
 
 // $(document).ready(function () {
@@ -19,3 +19,48 @@
 //     let box2 = $("#box-2").html()
 //     console.log(box2);
 // })
+<<<<<<< Updated upstream
+=======
+
+// ************************************** Jquery Selectors ************************************** 
+
+// (1) Select By Id: $("#idName")
+// (2) Select By Class: $("#className")
+// (3) Select By Tag: $("#tagName")
+// (4) Select All Element: $("*")
+// (4) Select Child Element In Class: $(".parentElementClassName .childElementClassName")
+// (4) Select Multiple Element: $(".elementOne , .elementTwo")
+
+
+$(document).ready(function () {
+    // let mainHeading = $("#mainHeading")
+    // let cardHeading = $(".card h3")
+    // let ul = $(".list-group")
+    // let li = $(".list-group .list-group-item")
+    // let liNumber = $(".list-group .list-group-item:nth-child(2)")
+    // let allElement = $("*")
+
+    // mainHeading.css("backgroundColor", "blue")
+
+    // cardHeading.css("color", "blue")
+
+    // ul.css("border", "2px solid blue")
+
+    // li.css("border", "2px solid blue")
+
+    // liNumber.css("border", "2px solid blue")
+
+    // allElement.css("border", "2px solid blue")
+})
+
+// ************************************** Jquery Mouse Events ************************************** 
+
+
+
+$(document).ready(function () {
+    // let btn = $(".btn-primary").click(function(){
+    //     let card = $(".card")
+    //     card.css("color", "#fff")
+    // })
+})
+>>>>>>> Stashed changes
